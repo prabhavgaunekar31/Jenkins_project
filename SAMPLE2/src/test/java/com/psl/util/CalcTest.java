9c9906ed53934666327e4a79d35c2b193862f5c3
prabhav <prabhavgaunekar31@gmail.com>
2018-01-15 15:58:26 +0530

    sample2 commit

---
+++ SAMPLE2/src/test/java/com/psl/util/CalcTest.java
@@ -0,0 +1,14 @@
+package com.psl.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalcTest {
+
+	@Test
+	public void test() {
+	assertEquals(5,Calc.add(2, 3));
+	}
+
+}
