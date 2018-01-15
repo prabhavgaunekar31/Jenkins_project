91995417e19160f5ec0a9e24a998ee33db4108ae
prabhav <prabhavgaunekar31@gmail.com>
2018-01-15 14:49:46 +0530

    First project commit

---
+++ Sample/src/com/psl/util/Calc.java
@@ -0,0 +1,14 @@
+package com.psl.util;
+
+
+public class Calc {
+	
+	private static int sum;
+	
+	public static int add(int a,int b){
+		sum=a+b;
+		return sum;
+	}
+
+}
+
---
+++ Sample/test/CalcTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.psl.util.Calc;
+
+
+public class CalcTest {
+
+	@Test
+	public void test() {
+	assertEquals(5,Calc.add(2, 3));
+	}
+
+}
